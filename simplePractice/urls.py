7c9b926814c830be724a0de524f43008d8787b14
jasper <63331153+jasper-oh@users.noreply.github.com>
2021-04-20 02:13:17 +0900

    Couldn't fix model, url

--- simplePractice/urls.py
+++ simplePractice/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from users import views
+
 urlpatterns = [
-    path('users/', include('users.urls')),
+    path('users/', views.index, name="users"),
     path('admin/', admin.site.urls),
 ]
